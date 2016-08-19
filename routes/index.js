afa6dc4e309a3a0ed52d2a92c8d2fd65891fdd54
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2016-08-19 19:26:05 +0200

    Fix undefined params

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const DeviceLoader = require('../controllers/device-loader');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express',
+    paramA: 'param:A',
+    paramC: 11,
+    paramObj: {x: 10},
+    paramObjX: {x: 'yo'},
+    deviceLoader: new DeviceLoader(),
+    devices: [
+      {id: 334, name: 'Note 5'},
+      {id: 445, name: 'Nexus 4'}
+    ]
+  });
 });
 
 module.exports = router;
