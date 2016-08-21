3d0147fcb9c4fd177f37c72138e2a27d84b8f291
Eugene Levenetc <shumvlesu@gmail.com>
2016-08-21 20:19:33 +0200

    Update view utils

--- utils/utils.js
+++ utils/utils.js
@@ -1,9 +1,18 @@
 /**
  * Created by eugene.levenetc on 19/08/16.
  */
-function duplicate(id, newId) {
-	var original = document.getElementById(id);
-	var clone = original.cloneNode(true);
-	clone.id = newId;
-	original.parentNode.appendChild(clone);
+function duplicate(containerId, device) {
+    var original = document.getElementById(containerId);
+    var line = document.createElement('p');
+    line.innerHTML = 'id:' + device.id + ' name: ' + device.name + '<button disabled=true>refreshing...</button>';
+    original.appendChild(line);
+}
+
+function setText(id, text) {
+    const element = document.getElementById(id);
+    element.innerHTML = text;
+}
+
+function remove(id){
+    document.getElementById(id).remove();
 }
