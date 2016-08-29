b83d4676dfe22000aa7b3876487c6605466c90ab
Eugene Levenetc <shumvlesu@gmail.com>
2016-08-29 23:10:21 +0200

    Remove local-settings

--- local-settings.js
+++ local-settings.js
@@ -4,5 +4,6 @@
 module.exports = {
     dbFile: 'device_manager',
     dbUser: 'root',
-    dbPass: 'Mysqlpass0'
+    dbPass: 'Mysqlpass0',
+    gcmApiKey: 'AIzaSyBB62ptu3eKvqHzH9xY7DzItjO3mgK2e3g'
 };
