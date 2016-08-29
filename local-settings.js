680ed177da0b8c8fad33d9cf7584e4e6bdb65e65
Eugene Levenetc <shumvlesu@gmail.com>
2016-08-29 23:17:23 +0200

    Remove local-settings

--- local-settings.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by eleven on 20/08/2016.
- */
-module.exports = {
-    dbFile: 'device_manager',
-    dbUser: 'root',
-    dbPass: 'Mysqlpass0',
-    gcmApiKey: 'AIzaSyBB62ptu3eKvqHzH9xY7DzItjO3mgK2e3g'
-};
