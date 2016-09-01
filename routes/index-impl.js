53d8a9f65ec0c80f4e2c0ecf0711de3cd2994381
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2016-09-01 23:24:17 +0200

    Refactor and clean up index routes

---
+++ routes/index-impl.js
@@ -0,0 +1,19 @@
+/**
+ * Created by eugene.levenetc on 01/09/16.
+ */
+
+const gcmSender = require('../gcm/gcm-sender');
+
+module.exports = {
+	ping: function (req, res) {
+		const device = req.body;
+		res.status(200);
+		res.send('');
+
+		gcmSender.ping(
+			device,
+			() => console.log('ping: ok'),
+			() => console.log('ping: fail')
+		);
+	}
+};
--- routes/index.js
+++ routes/index.js
@@ -4,76 +4,64 @@ const DeviceLoader = require('../controllers/device-loader');
 const database = require('../database/database-sequelize');
 const winston = require('winston');
 const gcmSender = require('../gcm/gcm-sender');
+const impl = require('./index-impl');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', {
-        title: 'Express',
-        paramA: 'param:A',
-        paramC: 11,
-        paramObj: {x: 10},
-        paramObjX: {x: 'yo'},
-        deviceLoader: new DeviceLoader(),
-        devices: [
-            {id: 334, name: 'Note 5'},
-            {id: 445, name: 'Nexus 4'}
-        ]
-    });
+	res.render('index', {
+		title: 'Express',
+		paramA: 'param:A',
+		paramC: 11,
+		paramObj: {x: 10},
+		paramObjX: {x: 'yo'},
+		deviceLoader: new DeviceLoader(),
+		devices: [
+			{id: 334, name: 'Note 5'},
+			{id: 445, name: 'Nexus 4'}
+		]
+	});
 });
 
 router.post('/ping', function (req, res) {
-    console.log('ping:');
-    const device = req.body;
-    console.log(device);
-    res.status(200);
-    res.send('');
-
-    gcmSender.ping(
-        device,
-        function () {
-            console.log('ping: ok');
-        }, function () {
-            console.log('ping: fail');
-        }
-    );
+	impl.ping(req, res);
 });
 
 router.post('/devices', function (req, res) {
-    const device = req.body;
-    console.log('devices:post', device);
-    database.createOrUpdate(device, function (result) {
-        console.log('devices:post:ok');
-        handleSuccess(res, {result: result});
-    }, function (error) {
-        console.log('devices:post:fail', error);
-        res.status(500);
-        res.send({result: error.message, device: device});
-    });
+	const device = req.body;
+	console.log('devices:post', device);
+	database.createOrUpdate(device, function (result) {
+		console.log('devices:post:ok');
+		handleSuccess(res, {result: result});
+	}, function (error) {
+		console.log('devices:post:fail', error);
+		res.status(500);
+		res.send({result: error.message, device: device});
+	});
 });
 
 router.get('/devices', function (req, res) {
 
-    database.getDevices(function (devices) {
-        handleSuccess(res, devices);
-    }, function (error) {
-        res.status(500);
-        res.send({result: error});
-    });
+	database.getDevices(function (devices) {
+		handleSuccess(res, devices);
+	}, function (error) {
+		res.status(500);
+		res.send({result: error});
+	});
 });
 
 router.delete('/devices', function (req, res) {
-    const id = req.query.deviceId
-    database.deleteDevice(id, function (result) {
-        handleSuccess(res, {deleted: result});
-    }, function (error) {
+	const id = req.query.deviceId
+	database.deleteDevice(id, function (result) {
+		handleSuccess(res, {deleted: result});
+	}, function (error) {
 
-    });
+	});
 });
 
 function handleSuccess(res, result) {
-    res.status(200);
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(result));
+	res.status(200);
+	res.setHeader('Content-Type', 'application/json');
+	res.send(JSON.stringify(result));
 }
 
 
