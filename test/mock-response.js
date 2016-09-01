d0b7981fd768bc4ca524db423f1cbb2a390d39b1
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2016-09-01 23:24:30 +0200

    Add index routes test

---
+++ test/mock-response.js
@@ -0,0 +1,11 @@
+/**
+ * Created by eugene.levenetc on 01/09/16.
+ */
+module.exports = {
+	status: function (code) {
+
+	},
+	send: function (body) {
+
+	}
+};
---
+++ test/routes-index.js
@@ -0,0 +1,23 @@
+/**
+ * Created by eugene.levenetc on 01/09/16.
+ */
+const assert = require('assert');
+const sinon = require('sinon');
+const mockRes = require('./mock-response');
+
+describe('Given foo requires the bar and path modules and bar.bar() returns "bar"', function () {
+	describe('When I resolve', function () {
+
+		let routes;
+
+		before(function () {
+			routes = require('../routes/index-impl');
+		});
+
+		it('foo is required 2 times', function () {
+			routes.ping({body: {}}, mockRes);
+			assert.notEqual(routes, null);
+		})
+
+	});
+});
