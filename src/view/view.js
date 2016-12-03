70a3cfcf87b2bc412de69893e645e99418cb6c64
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2016-12-03 22:56:33 +0100

    Add os param filters

--- src/view/view.js
+++ src/view/view.js
@@ -5,9 +5,10 @@
 const utils = require('../utils/utils');
 
 const boolFieldsMap = {
-    'ble': 'hasBluetoothLowEnergy',
-    'bt': 'hasBluetooth',
-    'nfc': 'hasNfc'
+    ble: 'hasBluetoothLowEnergy',
+    bt: 'hasBluetooth',
+    nfc: 'hasNfc',
+    online: 'isOnline'
 };
 
 exports.renderDevices = function (type, devices, verbose) {
@@ -16,11 +17,94 @@ exports.renderDevices = function (type, devices, verbose) {
     } else if (type === 'json') {
         return devices;
     } else {
-        return 'Invalid view type: ' + type;
+        return `Invalid view type param: ${type}`;
     }
 };
 
-function slack(devices, verbose, filter) {
+/**
+ *
+ * @param devices
+ * @param {string} [rawFilters]
+ * @return {*}
+ */
+exports.filterDevices = function (devices, rawFilters) {
+
+    if (!utils.isDefined(rawFilters)) return devices;
+
+    const filters = rawFilters.split(',');
+
+    if (filters.length == 0) return devices;
+
+    for (let i = 0; i < filters.length; i++) {
+        let d = devices.length;
+        const fKey = filters[i].split(':')[0];
+        const fValue = filters[i].split(':')[1];
+        while (d--) {
+
+            if (boolFieldsMap.hasOwnProperty(fKey)) {
+                const devKey = boolFieldsMap[fKey];
+                const filterValue = Boolean(fValue);
+                let device = devices[d];
+                if (device.hasOwnProperty(devKey) && device[devKey] !== filterValue) {
+                    devices.splice(d, 1);
+                    continue;
+                }
+            }
+
+            if (fKey === 'os') {
+
+            }
+        }
+    }
+
+    return devices;
+};
+
+exports.isMatch = function (dbVersion, filterPattern) {
+    dbVersion = fixDbVersion(dbVersion);
+    if (!utils.isDefined(filterPattern)) return false;
+    if (filterPattern === '') return false;
+    if (dbVersion === filterPattern) return true;
+    const regex = new RegExp(filterPattern.replace(/\./g, 'V').replace(`\*`, '.{1,}'));
+    dbVersion = dbVersion.replace(/\./g, 'V');
+    let result = dbVersion.match(regex);
+    return result != null;
+};
+
+exports.isValidOsFilter = function (filterPattern) {
+
+    const aPattern = /^(\d|\*)$/;
+    const abPattern = /^(\d|\*)\.(\d|\*)$/;
+    const abcPattern = /^(\d|\*)\.(\d|\*)\.(\d|\*)$/;
+
+    const checkA = filterPattern.match(aPattern);
+    const checkAB = filterPattern.match(abPattern);
+    const checkABC = filterPattern.match(abcPattern);
+
+    if (checkA != null) {
+        return true;
+    } else if (checkAB != null) {
+        return true;
+    } else if (checkABC != null) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+exports.fixDbVersion = fixDbVersion;
+
+function fixDbVersion(ver) {
+    if (ver.length == 1) {
+        return `${ver}.0.0`;
+    } else if (ver.length == 3) {
+        return `${ver}.0`;
+    } else {
+        return ver;
+    }
+}
+
+function slack(devices, verbose) {
     let result = '';
     if (devices.length == 0) {
         result = 'No devices'
@@ -28,7 +112,6 @@ function slack(devices, verbose, filter) {
         for (let i = 0; i < devices.length; i++) {
 
             let device = devices[i];
-            let skippedByFiler = false;
 
             let deviceId = device.deviceId;
             let deviceName = utils.capitalize(device.manufacturer + ' ' + device.model);
@@ -46,21 +129,6 @@ function slack(devices, verbose, filter) {
             let hasFingerprintScanner = device.hasFingerprintScanner;
             let batteryLevel = device.batteryLevel;
             let isOnline = device.isOnline ? ':new_moon:' : ':full_moon:';
-
-            if (utils.isDefined(filter)) {
-                Object.keys(filter).forEach(function (key) {
-                    if (typeof filter[key] === 'boolean') {
-                        if (boolFieldsMap.hasOwnProperty(key)) {
-                            if (!this[boolFieldsMap[key]]) {
-                                skippedByFiler = true;
-                                break;
-                            }
-                        }
-                    }
-                });
-            }
-
-            if (skippedByFiler) continue;
 
             if (verbose) {
                 result +=
---
+++ test/view-tests.js
@@ -0,0 +1,88 @@
+/**
+ * Created by eugene.levenetc on 03/12/2016.
+ */
+const assert = require('assert');
+const sinon = require('sinon');
+const view = require('../src/view/view');
+
+describe('View utils', function () {
+
+    before(function () {
+
+    });
+
+    it('with null filter should return same devices', function () {
+        const devices = [{hasBluetoothLowEnergy: false}, {hasBluetoothLowEnergy: true}];
+        const filtered = view.filterDevices(devices, null);
+        assert.deepEqual(devices, filtered);
+    });
+
+    it('with undefined filter should return same devices', function () {
+        const devices = [{hasBluetoothLowEnergy: false}, {hasBluetoothLowEnergy: true}];
+        const filtered = view.filterDevices(devices);
+        assert.deepEqual(devices, filtered);
+    });
+
+    it('all devices should be filtered out', function () {
+        const devices = [{hasBluetoothLowEnergy: false}, {hasBluetoothLowEnergy: false}];
+        const filtered = view.filterDevices(devices, 'ble:true');
+        assert.deepEqual([], filtered);
+    });
+
+    it('all devices should not be filtered out', function () {
+        const devices = [{hasBluetoothLowEnergy: false}, {hasBluetoothLowEnergy: false}];
+        const filtered = view.filterDevices(devices, 'ble:false');
+        assert.deepEqual(devices, filtered);
+    });
+
+    it('test invalid os filters', function () {
+        assert.equal(view.isValidOsFilter('6.b.0'), false);
+        assert.equal(view.isValidOsFilter('6.*.x'), false);
+        assert.equal(view.isValidOsFilter('v.*.*'), false);
+
+        assert.equal(view.isValidOsFilter('660'), false);
+        assert.equal(view.isValidOsFilter('6c*'), false);
+        assert.equal(view.isValidOsFilter('f'), false);
+
+        assert.equal(view.isValidOsFilter(''), false);
+        assert.equal(view.isValidOsFilter('f.f.f'), false);
+    });
+
+    it('test valid os filters', function () {
+        assert.equal(view.isValidOsFilter('*.0.0'), true);
+        assert.equal(view.isValidOsFilter('6.0.0'), true);
+        assert.equal(view.isValidOsFilter('6.*.0'), true);
+        assert.equal(view.isValidOsFilter('*.*.*'), true);
+
+        assert.equal(view.isValidOsFilter('6.0'), true);
+        assert.equal(view.isValidOsFilter('6.*'), true);
+        assert.equal(view.isValidOsFilter('*.*'), true);
+
+        assert.equal(view.isValidOsFilter('6'), true);
+        assert.equal(view.isValidOsFilter('*'), true);
+    });
+
+    it('all 6.* devices should filtered', function () {
+
+        assert.equal(view.isMatch('6', '6'), true);
+        assert.equal(view.isMatch('6', '*'), true);
+        assert.equal(view.isMatch('6', '6.*'), true);
+
+        assert.equal(view.isMatch('6.0', '*'), true);
+        assert.equal(view.isMatch('6.0', '6'), true);
+        assert.equal(view.isMatch('6.0', '6.'), true);
+
+        assert.equal(view.isMatch('6.0.3', '*'), true);
+        assert.equal(view.isMatch('6.0.3', '6'), true);
+        assert.equal(view.isMatch('6.0.3', '6.*'), true);
+        assert.equal(view.isMatch('6.0.3', '6.*.*'), true);
+        assert.equal(view.isMatch('6.0.3', '6.*.3'), true);
+        assert.equal(view.isMatch('6.0.3', '6.0.3'), true);
+    });
+
+    it('fix db version', function () {
+        assert.equal(view.fixDbVersion('6'), '6.0.0');
+        assert.equal(view.fixDbVersion('6.1'), '6.1.0');
+        assert.equal(view.fixDbVersion('6.1.2'), '6.1.2');
+    });
+});
